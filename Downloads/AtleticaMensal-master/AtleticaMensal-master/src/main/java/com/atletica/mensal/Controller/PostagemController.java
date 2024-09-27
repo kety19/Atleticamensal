364d93d9895795212df7de04a57de6008946d51a
kety19 <ketelnsilva358@gmail.com>
2024-09-27 17:31:09 -0300

    update postagemControler

--- Downloads/AtleticaMensal-master/AtleticaMensal-master/src/main/java/com/atletica/mensal/Controller/PostagemController.java
+++ Downloads/AtleticaMensal-master/AtleticaMensal-master/src/main/java/com/atletica/mensal/Controller/PostagemController.java
@@ -11,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.atletica.mensal.Entities.AtleticaEntity;
 import com.atletica.mensal.Entities.PostagemEntity;
 import com.atletica.mensal.Entities.UserEntity;
+import com.atletica.mensal.Repository.AtleticaRepository;
+import com.atletica.mensal.Repository.PostagemRepository;
 import com.atletica.mensal.Service.PostagemService;
 import com.atletica.mensal.Service.UserService;
 
@@ -22,6 +25,12 @@ public class PostagemController {
 
 	@Autowired
 	private PostagemService postagemService;
+	
+	@Autowired
+	private AtleticaRepository atleticaRepository;
+	
+	@Autowired
+	private PostagemRepository postagemRepository;
 
 	@Autowired
 	private UserService userService;
@@ -31,8 +40,23 @@ public class PostagemController {
 		UserEntity user = userService.findByEmail(email);
 		return postagemService.salvarPostagem(postagem, user);
 	}
+	
+/*	public PostagemEntity criarPostagem(long atletica_id, PostagemEntity postagem, long user_id)   {
+        AtleticaEntity atletica = atleticaRepository.findById(atletica_id)
+                .orElseThrow(() -> new RuntimeException("Atlética não encontrada"));
 
-	@GetMapping
+        // Verificar se o usuário tem permissão para postar 
+        if (!atletica.getDonoId().equals(user_id)) {
+            throw new RuntimeException("Usuário não autorizado a postar");
+        }
+
+        // Associar a postagem à atlética
+ 
+		postagem.setAtletica(atletica);
+		return postagemRepository.save(postagem);
+           }
+*/
+	@GetMapping(value = "/listarPostagemPorAtleticas")
 	public List<PostagemEntity> listarPostagensPorAtletica(@PathVariable long atletica_id) {
 		return postagemService.listarPostagensPorAtletica(atletica_id);
 	}
